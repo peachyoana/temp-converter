1d4e6048ba6f224b1475189ea8ca2a92ff5f0082
peachyoana <ioni.borislavova@yahoo.com>
2023-01-21 00:17:05 +0000

    Created new class for gradient

--- src/TempConverter.java
+++ src/TempConverter.java
@@ -3,10 +3,26 @@ import java.awt.*;
 
 public class TempConverter extends JFrame {
 	ImageIcon image; 
+	JPanel panel; 
+	JPanelGradient hotGradient; 
+	JPanelGradient coldGradient; 
+	
+	
+	
 	public TempConverter() {  
-		image = new ImageIcon("src/img/icon.png");
+		// define variables
+		hotGradient = new JPanelGradient(Color.ORANGE, Color.RED); // creates background gradient 
+		coldGradient = new JPanelGradient(new Color(0x6DD5FA), new Color(0x2980B9));// creates background gradient 
+
+		image = new ImageIcon("src/img/icon.png"); // file path to program icon
+		
+
 		this.setIconImage(image.getImage()); // changes icon of frame
-		this.getContentPane().setBackground(new Color(255,100,0));
+
+		// Add components
+		add(coldGradient);
 		
 	}
+
+	
 }
